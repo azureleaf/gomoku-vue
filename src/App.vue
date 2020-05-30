b32360a5a472231dcdcbf8b9af1143a500d186e4
akb <doge.wow.so.scare@gmail.com>
2020-05-30 19:31:33 +0900

    feat: Add vuetify with vue-cli

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,56 @@
 <template>
-  <div id="app">
-    <h1>{{ title }}</h1>
-    <Board></Board>
-    <Panel></Panel>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Board from "./components/Board";
-import Panel from "./components/Panel";
+import HelloWorld from "./components/HelloWorld";
 
 export default {
   name: "App",
+
   components: {
-    Board,
-    Panel,
+    HelloWorld,
   },
-  data: function() {
-    return {
-      title: "Gomoku",
-    };
-  },
-  mounted: function() {
-    // Initialize the Vuex state
-    this.$store.commit("initBoard");
-  },
+
+  data: () => ({
+    //
+  }),
 };
 </script>
-
-<style lang="stylus"></style>
