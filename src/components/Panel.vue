4d52b37ee5e8503fd0a67db4ecddcc515f3d45f2
akb <doge.wow.so.scare@gmail.com>
2020-05-31 12:28:08 +0900

    feat: Limit the chain length equal to or shorter than board size

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -37,23 +37,25 @@
       class="mt-5 pt-5"
       v-model="boardSize"
       step="1"
-      :max="20"
-      :min="3"
+      :max="boardSizeLimit.max"
+      :min="boardSizeLimit.min"
       thumb-label="always"
       :color="uniColor"
       :track-color="uniColor"
       :thumb-color="uniColor"
       ticks="always"
       tick-size="4"
+      @change="updateChainLengthLimits()"
     ></v-slider>
+
     <p>Chain Length</p>
 
     <v-slider
       class="mt-5 pt-5"
       v-model="chainLength"
       step="1"
-      :max="7"
-      :min="3"
+      :max="chainLengthLimit.max"
+      :min="chainLengthLimit.min"
       thumb-label="always"
       :color="uniColor"
       :track-color="uniColor"
@@ -71,6 +73,8 @@ export default {
       boardSize: 10,
       chainLength: 5,
       uniColor: "deep-orange",
+      chainLengthLimit: { max: 7, min: 3 },
+      boardSizeLimit: { max: 20, min: 3 },
     };
   },
   computed: {
@@ -79,6 +83,33 @@ export default {
     },
     activePlayer: function() {
       return this.$store.state.isOsTurn ? "O" : "X";
+    },
+  },
+  methods: {
+    // Keep the chain length equal to or shorter than the board length
+    updateChainLengthLimits() {
+      if (this.boardSize < 7) {
+        /**
+         * When there's only one selectable value in v-slider,
+         * such as "3" chain length in 3x3 board,
+         * position of the slider picker remains at the last position,
+         * which looks ugly and confusing for users
+         * So in that case, put the picker in the leftmost position in advance.
+         * And this preprocess needs to be done asynchnously
+         */
+        Promise.resolve()
+          .then(() => {
+            // Put the picker to the leftmost edge
+            if (this.boardSize == 3) this.chainLength = 3;
+          })
+          .then(() => {
+            this.chainLengthLimit.max = this.boardSize;
+            if (this.chainLength > this.boardSize)
+              this.chainLength = this.boardSize;
+          });
+      } else {
+        this.chainLengthLimit.max = 7;
+      }
     },
   },
 };
