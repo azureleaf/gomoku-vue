0f9116c9ddbd5205a8b126314243ada0a98453c4
akb <doge.wow.so.scare@gmail.com>
2020-05-31 10:28:19 +0900

    style: Set button width, set slider ticks
    Override the Vuetify internal class for the color & shape of slider ticks

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -8,12 +8,30 @@
       Turn:
       <span class="largeText ml-1">{{ activePlayer }}</span>
     </p>
-    <v-btn class="disableCapsLock ma-1" depressed :color="uniColor" dark>
-      <v-icon class="mr-1">mdi-arrow-left-drop-circle</v-icon>One Move Back
-    </v-btn>
-    <v-btn class="disableCapsLock ma-1" depressed :color="uniColor" dark>
-      <v-icon class="mr-1">mdi-reload</v-icon>Resume
-    </v-btn>
+    <v-row>
+      <v-col cols="12" md="6" class="px-1">
+        <v-btn
+          class="disableCapsLock ma-1"
+          depressed
+          :color="uniColor"
+          block
+          dark
+        >
+          <v-icon class="mr-1">mdi-arrow-left-drop-circle</v-icon>Back
+        </v-btn>
+      </v-col>
+      <v-col cols="12" md="6" class="px-1">
+        <v-btn
+          class="disableCapsLock ma-1"
+          depressed
+          :color="uniColor"
+          block
+          dark
+        >
+          <v-icon class="mr-1">mdi-reload</v-icon>Reset
+        </v-btn>
+      </v-col>
+    </v-row>
     <p>Board Size</p>
     <v-slider
       class="mt-5 pt-5"
@@ -25,6 +43,8 @@
       :color="uniColor"
       :track-color="uniColor"
       :thumb-color="uniColor"
+      ticks="always"
+      tick-size="4"
     ></v-slider>
     <p>Chain Length</p>
 
@@ -32,12 +52,14 @@
       class="mt-5 pt-5"
       v-model="chainLength"
       step="1"
-      :max="10"
+      :max="7"
       :min="3"
       thumb-label="always"
       :color="uniColor"
       :track-color="uniColor"
       :thumb-color="uniColor"
+      ticks="always"
+      tick-size="4"
     ></v-slider>
   </v-container>
 </template>
@@ -64,5 +86,11 @@ export default {
 <style scoped lang="stylus">
 .largeText {
   font-size: 1.4em;
+}
+
+// slider ticks: override the internal Vuetify class
+::v-deep .v-slider__tick--filled, ::v-deep .v-slider__tick {
+  background-color: #ff5722 !important;
+  border-radius: 2px !important;
 }
 </style>
