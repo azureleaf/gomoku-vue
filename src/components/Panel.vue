c3b0fecb77fbabb179086420978ce24b89a5e419
akb <doge.wow.so.scare@gmail.com>
2020-06-24 13:42:49 +0900

    feat: Show reset confirmation dialog on change of the slider
    Formerly, it just showed snackbar to urge the user to reset the game

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -41,17 +41,6 @@
       <span class="largeText ml-1">{{ activePlayer }}</span>
     </p>
     <v-row>
-      <!-- <v-col cols="12" md="6" class="px-1">
-        <v-btn
-          class="disableCapsLock ma-1"
-          depressed
-          :color="uniColor"
-          block
-          dark
-        >
-          <v-icon class="mr-1">mdi-arrow-left-drop-circle</v-icon>Back
-        </v-btn>
-      </v-col> -->
       <v-col cols="12" class="px-1">
         <v-btn
           class="disableCapsLock ma-1"
@@ -82,23 +71,6 @@
       tick-size="5"
       @change="updateChainLengthLimits()"
     ></v-slider>
-
-    <!-- <p>Chain Length</p> -->
-
-    <!-- <v-slider
-      class="mt-5 pt-5"
-      v-model="chainLength"
-      step="1"
-      :max="chainLengthLimit.max"
-      :min="chainLengthLimit.min"
-      thumb-label="always"
-      :color="uniColor"
-      :track-color="uniColor"
-      :thumb-color="uniColor"
-      ticks="always"
-      tick-size="5"
-      @change="openSnackbar()"
-    ></v-slider> -->
     <v-snackbar v-model="isSnackbarOpen" :timeout="10000" color="grey darken-3">
       To apply changes, please reset the game!
       <v-btn color="deep-orange" text @click="isSnackbarOpen = false">
@@ -115,11 +87,8 @@ export default {
       boardSize: null,
       chainLength: 5,
       uniColor: "deep-orange",
-      chainLengthLimit: { max: 5, min: 3 },
       boardSizeLimit: { max: 20, min: 5 },
       isDialogOpen: false,
-      isSnackbarOpen: false,
-      hasShownSnackbar: false,
     };
   },
   computed: {
@@ -133,38 +102,11 @@ export default {
   methods: {
     // Keep the chain length equal to or shorter than the board length
     updateChainLengthLimits() {
-      if (this.boardSize < 5) {
-        /**
-         * When there's only one selectable value in v-slider,
-         * such as "3" chain length in 3x3 board,
-         * position of the slider picker remains at the last position,
-         * which looks ugly and confusing for users
-         * So in that case, put the picker in the leftmost position in advance.
-         * And this preprocess needs to be done asynchnously
-         */
-        Promise.resolve()
-          .then(() => {
-            // Put the picker to the leftmost edge
-            if (this.boardSize == 3) this.chainLength = 3;
-          })
-          .then(() => {
-            this.chainLengthLimit.max = this.boardSize;
-            if (this.chainLength > this.boardSize)
-              this.chainLength = this.boardSize;
-          });
-      } else {
-        this.chainLengthLimit.max = 5;
-      }
-      this.openSnackbar();
+      this.isDialogOpen = true;
     },
     handleGameResetBtn() {
       this.isDialogOpen = false;
       this.$store.commit("updateBoardSize", { boardSize: this.boardSize });
-    },
-    openSnackbar() {
-      if (this.hasShownSnackbar) return;
-      this.hasShownSnackbar = true;
-      this.isSnackbarOpen = true;
     },
   },
   mounted() {
